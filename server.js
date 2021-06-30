203039f358f8b83f05f012c3d8d66fdb1335e0df
Margaux Mansanarez <margaux.mansanarez@gmail.com>
2021-06-30 12:05:46 +0200

    Display blog posts

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const formidable = require('express-formidable');
 const fs = require('fs');
-const app = express();
 
-fs.readFile(__dirname + '/data/posts.json', function(error, file) {
-  const parsedFile = JSON.parse(file);
-  console.log(parsedFile);
-});
+const app = express();
 
 app.use(express.static("public"));
 
 app.use(formidable());
 
+app.get("/get-posts", function(req, res) {
+  res.sendFile(__dirname + '/data/posts.json');
+});
+
 app.post("/create-post", function(req, res) {
-  console.log(req.fields);
+
+  fs.readFile(__dirname + '/data/posts.json', function(error, file) {
+    if (error) {
+        console.log(error);
+    } else {
+        const content = req.fields.blogpost;
+        const timestamp = Date.now().toString();
+
+        const parsedFile = JSON.parse(file);
+        parsedFile[timestamp] = content;
+
+        const stringifiedFile = JSON.stringify(parsedFile);
+
+        fs.writeFile(__dirname + '/data/posts.json', stringifiedFile, function(error) {
+          if (error) {
+              console.log(error);
+          } else {
+              res.redirect('/');
+          }
+        });
+    };
+  });
 });
 
 app.listen(3000, function() {
